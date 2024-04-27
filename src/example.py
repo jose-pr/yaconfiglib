0c00e0c62048542b7f3f2061b16a54a919fc5aed
Jose A. <jose-pr@coqui.dev>
2024-04-27 04:13:24 -0400

    toml

--- src/example.py
+++ src/example.py
@@ -3,20 +3,21 @@ import sys
 
 import yaml
 
-from yaconfiglib.jinja2 import Jinja2Reader
+from yaconfiglib.jinja2 import *
+from yaconfiglib.toml import *
 from yaconfiglib.yaml import *
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-include = Include()
+loader = Include()
 
-yaml.SafeLoader.add_constructor("!include", include)
+yaml.SafeLoader.add_constructor("!include", loader)
 
 
 config = yaml.safe_load(
     "test: !include {pathname: examples/includeme.yaml, transform: '{ pathname.name: value.include }', key_factory: '%pathname.as_posix()', type: map }"
 )
 
-jinjaconfig = yaml.safe_load("test: !include examples/jinja.yaml.j2")
-
+jinjaconfig = loader.load("examples/jinja.yaml.j2")
+pyproject = loader.load("pyproject.toml")
 pass
