aceb333bc02f83064ef22a0ca13b303d0ca00831
jose-pr <jose-pr@coqui.dev>
2024-04-27 00:27:47 -0400

    initial

---
+++ src/example.py
@@ -0,0 +1,17 @@
+from yaconfiglib.yaml.include import Include
+from yaconfiglib.yaml.reader import Reader
+import yaml
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+include = Include()
+
+yaml.SafeLoader.add_constructor('!include', include)
+
+
+
+config = yaml.safe_load("test: !include examples/includeme.yaml ")
+
+pass
