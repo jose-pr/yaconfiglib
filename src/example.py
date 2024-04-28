aeaa5e53ff37dd7c2e73f9a23a4ebbce69f74177
jose-pr <jose-pr@coqui.dev>
2024-04-28 13:03:33 -0400

    Merge update

--- src/example.py
+++ src/example.py
@@ -1,12 +1,35 @@
 import logging
 import sys
+from dataclasses import dataclass
 
 import yaml
 
 from yaconfiglib.hiera import HieraConfigLoader, LogLevel, MergeMethod
 from yaconfiglib.jinja2 import *
 from yaconfiglib.toml import *
+from yaconfiglib.utils.merge import typed_merge
 from yaconfiglib.yaml import *
+
+
+@dataclass
+class Test:
+    field_1: str
+    field_2: int
+    field_3: str
+
+    def __init__(self, **kwargs):
+        for arg in kwargs:
+            setattr(self, arg, kwargs[arg])
+        self.field_4 = f"{self.field_1}_{self.field_2}"
+
+
+merged = typed_merge(
+    Test,
+    Test(field_1=11, field_2=22, field_3=33),
+    dict(field_1=1, field_2=2),
+    init=True,
+)
+
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
